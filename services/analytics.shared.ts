56a53707574d908705074287edd5b3dba473b103
nelson <nelson.ganlw@gmail.com>
2026-06-23 16:18:53 +0800

    chore(analytics): remove screen/tab view tracking, rename widget event

--- services/analytics.shared.ts
+++ services/analytics.shared.ts
@@ -21,9 +21,8 @@ export const AnalyticsEvents = {
   ONBOARDING_NOTIFICATIONS_ENABLED: 'Onboarding Notifications Enabled',
   ONBOARDING_NOTIFICATIONS_SKIPPED: 'Onboarding Notifications Skipped',
 
-  // Navigation / screen views
-  TAB_VIEWED: 'Tab Viewed',
-  SCREEN_VIEWED: 'Screen Viewed',
+  // Home-screen widget deep-link opens
+  WIDGET_OPENED: 'Widget Opened',
 
   // Transactions
   TRANSACTION_CREATED: 'Transaction Created',
